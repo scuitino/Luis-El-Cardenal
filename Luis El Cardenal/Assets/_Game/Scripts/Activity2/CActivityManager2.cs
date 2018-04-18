9bf58e33dac1c446f20d4c5ee35444a76dfb94c0
scuitino <sebastiancuitino@gmail.com>
2018-04-17 22:22:51 -0300

    Prototype Working with one word
    TODO: Animations

---
+++ Luis El Cardenal/Assets/_Game/Scripts/Activity2/CActivityManager2.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CActivityManager2 : MonoBehaviour {
+
+    // list of all the words available on this activity
+    [SerializeField]
+    List<CWordA2> _wordsList;
+
+    // answer of the actual question 
+    [SerializeField]
+    int _playerAnswer;
+
+    // actual question word
+    CWordA2 _actualWord;
+
+    private void Start()
+    {
+        PlayWord();
+    }
+
+    // Play with the next word
+    public void PlayWord()
+    {
+        _playerAnswer = 0; // reset the answer
+        bool tReady = false;
+        while (!tReady) // while the next word is not selected
+        {
+            CWordA2 tSelectedWord = _wordsList[Random.Range(0, _wordsList.Count)];
+            if (!tSelectedWord._wasUsed) //  is the word was not used
+            {
+                _actualWord = tSelectedWord;
+                tReady = true; // ready to play
+            }
+        }        
+        // TODO: play animation of the word
+    }
+
+    // to add 1 to syllables answer
+    public void AddSyllable()
+    {
+        _playerAnswer++;
+    }
+
+    // to remove 1 to syllables answer
+    public void RemoveSyllable()
+    {
+        _playerAnswer--;
+    }
+
+    // check result of the actual question
+    public void CheckResult()
+    {
+        if (_playerAnswer == _actualWord._numberOfSyllables)
+        {
+            Debug.Log("Respuesta correcta.");
+        }
+        else
+        {
+            Debug.Log("Respuesta incorrecta, intentalo de nuevo.");
+        }
+    }
+}
---
+++ Luis El Cardenal/Assets/_Game/Scripts/Activity2/SO/CWordA2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Word", menuName = "Activities/Activity2/Word")]
+public class CWordA2 : ScriptableObject {
+
+    public string _word;                    // name of the object
+    public List<string> _syllables;         // all the syllables of the word
+    public Sprite _image;                   // sprite of the object
+    public int _numberOfSyllables;          // number of syllables of the word    
+    public bool _wasUsed;                   // to know if the word was used
+}
