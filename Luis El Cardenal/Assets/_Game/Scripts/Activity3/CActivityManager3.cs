d527e10e13374fd3acbe0f52a75420dd9b17600d
scuitino <sebastiancuitino@gmail.com>
2018-05-14 17:49:56 -0300

    Sprites added, prefabs and scriptableObject created

--- Luis El Cardenal/Assets/_Game/Scripts/Activity3/CActivityManager3.cs
+++ Luis El Cardenal/Assets/_Game/Scripts/Activity3/CActivityManager3.cs
@@ -5,13 +5,38 @@ using UnityEngine;
 
 public class CActivityManager3 : CActivity {
 
+    #region SINGLETON PATTERN
+    public static CActivityManager3 _instance = null; //static - the same variable is shared by all instances of the class that are created
+    #endregion
+
+    // syllables for all questions on this level
+    [SerializeField, Header("Configuration")]
+    List<GameObject> _challengesOnThisLevel;
+
+    // actual question on the _syllablesOnThisLevel list
+    int _actualChallenge;
+
     // result slots
-    [SerializeField]
-    DragAndDropCell _flower1, _flower2;
+    [SerializeField, Header("References")]
+    DragAndDropCell _flower1;
+    DragAndDropCell _flower2;
 
     // to move result button
     [SerializeField]
     DOTweenAnimation _resultButton;
+
+    // to animate the rabbit
+    [SerializeField]
+    Animator _rabbitAnimator;
+
+    private void Awake()
+    {
+        //Singleton check
+        if (_instance == null)
+            _instance = this;
+        else if (_instance != this)
+            Destroy(gameObject);
+    }
 
     private void OnEnable()
     {
@@ -28,6 +53,12 @@ public class CActivityManager3 : CActivity {
         }
     }
 
+    // Play with the next word
+    public void PlayWord()
+    {
+        
+    }
+
     // check result of the actual question
     public void CheckResult()
     {
@@ -38,10 +69,12 @@ public class CActivityManager3 : CActivity {
                 _resultButton.DOPlayBackwards();
                 if (_flower1.GetItem().GetItemID() == _flower2.GetItem().GetItemID())
                 {
+                    _rabbitAnimator.SetTrigger("Success");
                     Debug.Log("son iguales");
                 }
                 else
                 {
+                    _rabbitAnimator.SetTrigger("Fail");
                     Debug.Log("son diferentes");
                 }
             }
---
+++ Luis El Cardenal/Assets/_Game/Scripts/Activity3/SO/CActivity3Challenge.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Challenge", menuName = "Activities/Activity3/Challenge")]
+public class CActivity3Challenge : ScriptableObject
+{
+    public GameObject _item1, _item2;           // correct items of this challenges
+    public bool _wasUsed;                       // to know if this challenge was used
+}
