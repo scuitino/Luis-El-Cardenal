826a25cb72db7d1e319d6019eb365308dfa80ec5
scuitino <sebastiancuitino@gmail.com>
2018-06-05 11:15:05 -0500

    Revert "Revert "Help working on A1""
    This reverts commit 034971bb4e6fa605d781f7869e90e76684d75843.

---
+++ Luis El Cardenal/Assets/_Game/Scripts/Activity1/CHelp1.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CHelp1 : MonoBehaviour {
+
+    // all the clips for the help animation
+    [SerializeField]
+    List<AudioClip> _helpSounds;
+
+    // to use with animation sounds
+    AudioSource _helpASource;
+
+    // to animate luis
+    [SerializeField]
+    Animator _luisAnimator;
+
+    // to animate the touched object
+    [SerializeField]
+    Animator _touchedAnimator;
+
+    private void Start()
+    {
+        _helpASource = this.GetComponent<AudioSource>();
+    }
+
+    // play the selected sound
+    public void PlayHelpSound(int aSound)
+    {
+        _helpASource.clip = _helpSounds[aSound];
+        _helpASource.Play();
+    }	
+
+    // start talking animation
+    public void StartTalking()
+    {
+        _luisAnimator.SetBool("Talking", true);
+    }
+
+    // stop talking animation
+    public void StopTalking()
+    {
+        _luisAnimator.SetBool("Talking", false);
+    }
+
+    // start success animation
+    public void StartSuccess()
+    {
+        _luisAnimator.SetTrigger("Success");
+    }
+
+    //  start touched object animation
+    public void StartTouchAnimation()
+    {
+        _touchedAnimator.SetTrigger("Play");
+    }
+}
