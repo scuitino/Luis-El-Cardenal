88eb2a681feda117e988d7caa075b74894693fc4
scuitino <sebastiancuitino@gmail.com>
2018-06-05 17:52:27 -0500

    Jump sounds, camalote color changed, continue button changed.

--- Luis El Cardenal/Assets/_Game/Scripts/Activity2/CFrog.cs
+++ Luis El Cardenal/Assets/_Game/Scripts/Activity2/CFrog.cs
@@ -8,10 +8,29 @@ public class CFrog : MonoBehaviour {
     [SerializeField]
     Animator _heartsAnimator;
 
+    // to play frog sounds
+    AudioSource _frogASource;
+
+    // list of frog sounds
+    [SerializeField]
+    List<AudioClip> _frogSounds;
+
+    private void Start()
+    {
+        _frogASource = this.GetComponent<AudioSource>();
+    }
+
     // to animate hearts
     public void PlayHearts()
     {
         _heartsAnimator.SetTrigger("Play");
+    }
+
+    // to play frog sounds
+    public void PlaySound(int aSound)
+    {
+        _frogASource.clip = _frogSounds[aSound];
+        _frogASource.Play();
     }
 
     // notify the manager that the jump animation is finished
