b44a214836fab00d0813eec2ab0ac75d3c982f80
scuitino <sebastiancuitino@gmail.com>
2018-08-01 12:54:54 -0500

    Activity 7 First Commit

---
+++ Luis El Cardenal/Assets/_Game/Scripts/Activity7/CActivityManager7.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CActivityManager7 : CActivity {
+
+    #region SINGLETON PATTERN
+    public static CActivityManager7 _instance = null;
+    #endregion
+
+    // answer of the actual question 
+    [SerializeField]
+    int _playerAnswer;
+
+    // to add 1 to word count
+    public void AddWordCount()
+    {
+        if (_readyToPlay && !_win)
+        {
+            //_whiteButtons[aButtonNumber].DORestart();
+            _playerAnswer++;
+        }
+    }
+
+    // to remove 1 to word count
+    public void RemoveWordCount()
+    {
+        if (_readyToPlay && !_win)
+        {
+            //_whiteButtons[aButtonNumber].DOPlayBackwards();
+            _playerAnswer--;
+        }
+    }
+}
---
+++ Luis El Cardenal/Assets/_Game/Scripts/Activity7/CEgg.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CEgg : MonoBehaviour {
+
+    // egg state
+    bool _isPressed;
+
+    // to modify the egg sprite
+    Image _eggImage;
+
+    // egg sprites
+    [SerializeField]
+    Sprite _selected, _unselected;
+
+    private void Start()
+    {
+        _eggImage = GetComponent<Image>();
+    }
+
+    // when the player touch the egg
+    public void PressEgg()
+    {
+        if (CActivityManager7._instance._readyToPlay)
+        {
+            if (!_isPressed)
+            {
+                CActivityManager7._instance.AddWordCount();
+                _eggImage.sprite = _selected;
+                _isPressed = true;
+            }
+            else
+            {
+                CActivityManager7._instance.RemoveWordCount();
+                _eggImage.sprite = _unselected;
+                _isPressed = false;
+            }
+        }        
+    }
+}
