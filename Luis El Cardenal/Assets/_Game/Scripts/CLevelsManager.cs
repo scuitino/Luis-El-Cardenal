6aeb6b4f953bbbad4a08a1948da6df59b217eed5
scuitino <sebastiancuitino@gmail.com>
2018-08-14 17:39:53 -0500

    Change color of the scenarios when are cleared

---
+++ Luis El Cardenal/Assets/_Game/Scripts/CLevelsManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CLevelsManager : MonoBehaviour {
+
+    #region SINGLETON PATTERN
+    public static CLevelsManager _instance = null;
+    #endregion
+
+    // true if the level is cleared
+    [SerializeField]
+    public bool _level1, _level2, _level3, _level4, _level5, _level6, _level7, _level8;
+
+    [SerializeField]
+    int _clearedLevel;
+
+    private void Awake()
+    {
+        //Singleton check
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(_instance);
+        }            
+        else if (_instance != this)
+            Destroy(gameObject);
+    }
+
+    public void CheckLevelsState()
+    {
+        if (_level1)
+        {
+            CMainMenu._instance.EnableColorSprite(1);
+        }
+        if (_level2)
+        {
+            CMainMenu._instance.EnableColorSprite(2);
+        }
+        if (_level3)
+        {
+            CMainMenu._instance.EnableColorSprite(3);
+        }
+        if (_level4)
+        {
+            CMainMenu._instance.EnableColorSprite(4);
+        }
+        if (_level5)
+        {
+            CMainMenu._instance.EnableColorSprite(5);
+        }
+        if (_level6)
+        {
+            CMainMenu._instance.EnableColorSprite(6);
+        }
+        if (_level7)
+        {
+            CMainMenu._instance.EnableColorSprite(7);
+        }
+        if (_level8)
+        {
+            CMainMenu._instance.EnableColorSprite(8);
+        }
+    }
+}
--- Luis El Cardenal/Assets/_Game/Scripts/CMainMenu.cs
+++ Luis El Cardenal/Assets/_Game/Scripts/CMainMenu.cs
@@ -1,9 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class CMainMenu : MonoBehaviour {
+
+    #region SINGLETON PATTERN
+    public static CMainMenu _instance = null;
+    #endregion
+
+    // to enable color sprite
+    [SerializeField]
+    Image _level1Image, _level2Image, _level3Image, _level4Image, _level5Image, _level6Image, _level7Image, _level8Image;
+
+    // color sprites
+    [SerializeField]
+    Sprite _level1Sprite, _level2Sprite, _level3Sprite, _level4Sprite, _level5Sprite, _level6Sprite, _level7Sprite, _level8Sprite;
+
+    private void Awake()
+    {
+        //Singleton check
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        CLevelsManager._instance.CheckLevelsState();
+    }
+
+    public void EnableColorSprite(int pLevel)
+    {
+        switch (pLevel)
+        {
+            case 1:
+                _level1Image.sprite = _level1Sprite;
+                break;
+            case 2:
+                _level2Image.sprite = _level2Sprite;
+                break;
+            case 3:
+                _level3Image.sprite = _level3Sprite;
+                break;
+            case 4:
+                _level4Image.sprite = _level4Sprite;
+                break;
+            case 5:
+                _level5Image.sprite = _level5Sprite;
+                break;
+            case 6:
+                _level6Image.sprite = _level6Sprite;
+                break;
+            case 7:
+                _level7Image.sprite = _level7Sprite;
+                break;
+            case 8:
+                _level8Image.sprite = _level8Sprite;
+                break;
+        }
+    }
 
     // applicaton quit
     public void ExitApplication()
