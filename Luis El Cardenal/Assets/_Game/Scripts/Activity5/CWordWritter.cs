7bbf8bc31211f151451262384f219ec649599a49
scuitino <sebastiancuitino@gmail.com>
2018-08-21 15:19:45 -0500

    Activity 5 word writers loaded

---
+++ Luis El Cardenal/Assets/_Game/Scripts/Activity5/CWordWritter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CWordWritter : MonoBehaviour {
+
+    [SerializeField]
+    string _word;
+
+    public string GetWord()
+    {
+        return _word;
+    }
+}
