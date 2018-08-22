b1d7e67449fc85f0c897d70d0caeb9e50b4b448a
scuitino <sebastiancuitino@gmail.com>
2018-08-22 18:41:19 -0500

    Activity 3 update

---
+++ Luis El Cardenal/Assets/_Game/Scripts/General/CPlayButtonSounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayButtonSounds : MonoBehaviour {
+
+    [SerializeField]
+    AudioSource _aSource;
+
+    [SerializeField]
+    int _activityNumber;
+
+    public void PlaySound()
+    {
+        switch (_activityNumber)
+        {
+            case 3:
+                if (CActivityManager3._instance._readyToPlay)
+                {
+                    _aSource.Play();
+                }
+                break;
+        }
+    }
+}
